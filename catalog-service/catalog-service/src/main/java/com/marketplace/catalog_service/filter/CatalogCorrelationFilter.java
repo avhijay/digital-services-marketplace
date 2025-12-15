d0a88a0fc25dbf10c2143a56d710a510ab2c8631
avhijay <parmaravhijay5049@gmail.com>
2025-12-15 23:39:14 +0530

    implement correlation-id filter with mdc and response in user , notification and catalog service

--- catalog-service/catalog-service/src/main/java/com/marketplace/catalog_service/filter/CatalogCorrelationFilter.java
+++ catalog-service/catalog-service/src/main/java/com/marketplace/catalog_service/filter/CatalogCorrelationFilter.java
@@ -1,35 +1,47 @@
-package com.marketplace.order_service.filter;
+package com.marketplace.catalog_service.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
+
 @Component
-public class CorrelationIdFilter extends OncePerRequestFilter {
-    private static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+public class CatalogCorrelationFilter extends OncePerRequestFilter implements Ordered {
+
+    private static final String CORRELATION_HEADER_ID = "X-Correlation-Id";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String correlationId = request.getHeader(CORRELATION_ID_HEADER);
+        String correlationId = request.getHeader(CORRELATION_HEADER_ID);
 
-        if(correlationId==null || correlationId.isBlank()){
+
+        if(correlationId==null||correlationId.isBlank()){
             correlationId= UUID.randomUUID().toString();
         }
 
-        MDC.put(CORRELATION_ID_HEADER,correlationId);
+        MDC.put(CORRELATION_HEADER_ID,correlationId);
+        response.setHeader(CORRELATION_HEADER_ID,correlationId);
 
         try{
             filterChain.doFilter(request,response);
-        }finally {
-            MDC.remove(CORRELATION_ID_HEADER);
+        }
+        finally {
+            MDC.remove(CORRELATION_HEADER_ID);
         }
 
+
+    }
+
+    @Override
+    public int getOrder() {
+        return -100;
     }
 }
