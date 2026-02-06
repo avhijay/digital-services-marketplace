d1e0d2f1cfe04c1c7004a196f941f8ec553ef050
avhijay <parmaravhijay5049@gmail.com>
2026-02-06 23:50:16 +0530

    implemented security config and OAuth2 skeleton

---
+++ api-gateway/api-gateway/src/main/java/com/marketplace/api_gateway/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.marketplace.api_gateway.security;
+
+
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain (ServerHttpSecurity security){
+
+
+        security.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange-> {
+                    exchange.pathMatchers("/auth/**").permitAll()
+                            .pathMatchers("/actuator/**").permitAll()
+                            .anyExchange().authenticated();
+
+
+                });
+        security.oauth2ResourceServer(oauth2->{
+            oauth2.jwt(jwt->{
+
+                jwt.jwtDecoder(jwtDecoder());
+            });
+        });
+
+        return security.build();
+
+
+    }
+
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(){
+
+
+        return NimbusReactiveJwtDecoder
+                .withJwkSetUri("")
+                .build();
+    }
+
+
+}
